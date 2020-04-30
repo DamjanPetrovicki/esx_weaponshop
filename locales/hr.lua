cc0d7282699d3eba30470c1cd84bb160e2892f0a
SyncEye <50116007+DamjanPetrovicki@users.noreply.github.com>
2020-04-30 15:18:22 +0200

    Added Croatian locale

---
+++ locales/hr.lua
@@ -0,0 +1,13 @@
+Locales ['hr'] = {
+  ['buy_license'] = 'Hoćete li kupiti dozvolu za oružje?',
+  ['yes'] = '%s',
+  ['no'] = 'Ne',
+  ['weapon_bought'] = 'Kupili ste oruzje za $%s',
+  ['not_enough_black'] = 'Nemate dovoljno prljavog novca',
+  ['not_enough'] = 'Nemate dovoljno novaca',
+  ['already_owned'] = 'Već imate to oružje!',
+  ['shop_menu_title'] = 'Oružarnica',
+  ['shop_menu_prompt'] = 'Pritisnite ~INPUT_CONTEXT~ da pristupite ~y~oružarnici~s~.',
+  ['shop_menu_item'] = '$%s',
+  ['map_blip'] = 'Oružarnica',
+}
